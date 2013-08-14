3e414ca9747115046949dcfddf5d166fa1f29758
Jordan Stone <jorstone@ubermind.com>
2013-08-14 09:50:52 -0600

    Initial Commit

---
+++ JSIAPManager/JSAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  JSAppDelegate.h
+//  JSIAPManager
+//
+//  Created by Stone, Jordan Matthew (US - Denver) on 8/14/13.
+//  Copyright (c) 2013 Jordan Stone. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface JSAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ JSIAPManager/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  JSIAPManager
+//
+//  Created by Stone, Jordan Matthew (US - Denver) on 8/14/13.
+//  Copyright (c) 2013 Jordan Stone. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "JSAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([JSAppDelegate class]));
+    }
+}
